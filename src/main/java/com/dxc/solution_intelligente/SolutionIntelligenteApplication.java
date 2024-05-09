741a267c57845b2dc64ddaeb6f5e93a30e4de846
Yahya <ybenaghmouch@gmail.com>
2024-05-09 11:34:23 +0100

    ggg

---
+++ src/main/java/com/dxc/solution_intelligente/SolutionIntelligenteApplication.java
@@ -0,0 +1,13 @@
+package com.dxc.solution_intelligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionIntelligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionIntelligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dxc/solution_intelligente/SolutionIntelligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.solution_intelligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionIntelligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
