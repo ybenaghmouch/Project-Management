5a643e8db31dc957e345502c931d19fa0f9661d7
Ybenaghmouch <Ybenaghmouch@gmail.com>
2024-05-20 03:26:15 +0100

    user 99%

--- Front/src/app/user-modal/user-modal.component.ts
+++ Front/src/app/user-modal/user-modal.component.ts
@@ -1,25 +1,28 @@
-import { Component, ElementRef, ViewChild, Input, EventEmitter, Output,OnChanges  } from '@angular/core';
+import { Component, ElementRef, ViewChild, Input, EventEmitter, Output  } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserModalService } from './service/user-modal.service';
+import { HttpClientModule } from '@angular/common/http';
 declare var bootstrap: any;
 
 @Component({
   selector: 'app-user-modal',
   standalone: true,
   imports: [NgIf, NgFor, FormsModule, ReactiveFormsModule],
+  providers: [UserModalService],
   templateUrl: './user-modal.component.html',
   styleUrls: ['./user-modal.component.css']
 })
 export class UserModalComponent  {
   form: FormGroup;
-
+  
   @Output() userAdded = new EventEmitter<any>();
 
   @ViewChild('userModal') userModal!: ElementRef;
   @Input() isEditMode: boolean =false;
   @Input() user: any;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,private userModalService:UserModalService) {
     this.form = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -51,13 +54,41 @@ export class UserModalComponent  {
   }
 
   submitForm() {
-    if (this.form.valid) {
+
+    if (this.user){
       const formData = this.form.value;
+      this.userModalService.putUsers(formData,formData.username,formData.role.toLowerCase()).subscribe(
+        (data: any) => {
+          this.user = data;
+        },
+        error => {
+          console.error('Error fetching users', error);
+        }
+      );
+      this.userAdded.emit({ ...this.form.value, username: this.user.username });
+      
+    }else{
+    if (this.form.valid) {
+      
+      const formData = this.form.value;
+      this.userModalService.postUsers(formData).subscribe(
+        (data: any) => {
+          this.user = data;
+        },
+        error => {
+          console.error('Error fetching users', error);
+        }
+      );
+      
+      
+      
+      
       this.userAdded.emit(formData);
       
-      console.log(formData);
-      this.form.reset();
-      this.closeModal();
     }
+    
+  }
+  this.form.reset();
+  this.closeModal();
   }
 }
