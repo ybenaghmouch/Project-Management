509b7f27bd7ce098fca16d65de7a1f4ae0c75e67
Ybenaghmouch <Ybenaghmouch@gmail.com>
2024-05-20 03:33:09 +0100

    user 99%

---
+++ Front/src/app/team-modal/team-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-modal.component.html',
+  styleUrl: './team-modal.component.css'
+})
+export class TeamModalComponent {
+
+}
